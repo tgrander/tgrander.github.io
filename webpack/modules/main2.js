b756ad0c0488f36dabfca986f6f398003d1dab74
Trey Granderson <treygranderson@gmail.com>
2017-05-25 23:02:29 -0400

    add all

---
+++ webpack/modules/main2.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+function Main2() {
+  return (
+    <div>
+
+      <div className="greeting centered">
+        Trey is a Front End Developer with an affinity for collaborating with brilliant minds to build beautiful things.
+      </div>
+
+      <section className="highlighted-work">
+        <div className="item" id="one"></div>
+        <div className="item" id="two"></div>
+        <div className="item" id="three"></div>
+      </section>
+
+      <section className="about-me">
+
+      </section>
+      <section className="experience">
+
+      </section>
+      <section className="contact">
+
+      </section>
+    </div>
+  )
+}
+
+export default Main2;
